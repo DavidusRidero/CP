a13a548c6e5da0c60a3cf083543abfb52a949b2b
VyayKumar <49907198+DavidusRidero@users.noreply.github.com>
2026-04-24 16:26:07 +0530

    P1 done.

--- ProjectEuler/Problem1/Problem_One.cpp
+++ ProjectEuler/Problem1/Problem_One.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 
 int main() {
-    std::cout << "hello";
+    const int n = 1000;
+    int sum = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        if (i%3 == 0)
+            sum+=i;
+        if (i%5 == 0)
+            sum+=i;
+        if (i%15==0)
+            sum-=i;
+    }
+    std::cout << "Sum of all the multiples of 3 or 5 below" << n << " : " << sum << "\n";
     return 0;
 }
